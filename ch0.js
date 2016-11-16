9e444171ef491cbf488e531eeadc5f219761ec02
bookair17 <kyoji-exterior@goo.jp>
2016-11-16 21:53:03 +0900

    update jsdoc

--- ch0.js
+++ ch0.js
@@ -1,5 +1,12 @@
 "use strict";
 
+/**
+ * @fileoverview Leap本用のCylonJsのヒナ型
+ * @copyright LeapMotion Developers Jp
+ * @author 
+ * @license MIT
+ * @version 1.0.0
+ */
 var Cylon = require("cylon");
 var TrackingHand = require("./lib/tracking_hand");
 
--- src/tracking_hand.es6
+++ src/tracking_hand.es6
@@ -1,8 +1,13 @@
 "use strict";
 
 /**
- * Tracking Hand by Leap Motion
+ * @fileoverview Tracking Hand by Leap Motion
+ * @copyright LeapMotion Developers Jp
+ * @author K90j1
+ * @license MIT
+ * @version 1.0.0
  */
+
 class TrackingHand {
 
   /**
